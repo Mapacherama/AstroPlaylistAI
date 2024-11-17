7185341691ee68fa1165fa27d6dfd1ff61a82aa1
Jerome Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2024-11-17 08:30:59 +0100

    Setting up some template code to visualize what I'm trying to achieve

---
+++ train_model.py
@@ -0,0 +1,48 @@
+import torch
+from torch.utils.data import DataLoader
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from datasets import Dataset
+
+data = {
+    "lyrics": [
+        "I'm a rolling thunder, a pouring rain",
+        "I see a little silhouetto of a man",
+        "Shake it off, shake it off",
+        "Back in black, I hit the sack",
+        "Take me down to the Paradise City"
+    ],
+    "labels": [1, 0, 0, 1, 1]
+}
+
+dataset = Dataset.from_dict(data)
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
+
+def tokenize_function(examples):
+    return tokenizer(examples["lyrics"], padding="max_length", truncation=True)
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
+
+training_args = TrainingArguments(
+    output_dir="./results",
+    evaluation_strategy="epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=16,
+    num_train_epochs=3,
+    weight_decay=0.01,
+    logging_dir="./logs",
+)
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
+
+results = trainer.evaluate()
+print(f"Evaluation Results: {results}")
