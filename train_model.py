9dbb4100f3c253dbaa2716268dec11583d9b7795
Jerome Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2024-11-17 09:57:10 +0100

    Implementing training data.

--- train_model.py
+++ train_model.py
@@ -1,28 +1,27 @@
 import torch
-from torch.utils.data import DataLoader
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
-from datasets import Dataset
-from sklearn.model_selection import train_test_split
+from datasets import load_dataset
 
-data = {
-    "lyrics": [
-        "I'm a rolling thunder, a pouring rain",
-        "I see a little silhouetto of a man",
-        "Shake it off, shake it off",
-        "Back in black, I hit the sack",
-        "Take me down to the Paradise City"
-    ],
-    "labels": [1, 0, 0, 1, 1]
-}
+ds = load_dataset("maharshipandya/spotify-tracks-dataset")
 
-dataset = Dataset.from_dict(data)
+def preprocess_spotify_dataset(example):
+    return {
+        "lyrics": example["track_name"],
+        "labels": 1 if example["popularity"] > 50 else 0
+    }
+
+processed_ds = ds["train"].map(preprocess_spotify_dataset, remove_columns=["track_name", "popularity"])
 
 tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
 
 def tokenize_function(examples):
-    return tokenizer(examples["lyrics"], padding="max_length", truncation=True)
+    return tokenizer(list(examples["lyrics"]), padding="max_length", truncation=True)
 
-tokenized_dataset = dataset.map(tokenize_function, batched=True)
+tokenized_dataset = processed_ds.map(tokenize_function, batched=True)
+
+split_dataset = tokenized_dataset.train_test_split(test_size=0.2)
+train_dataset = split_dataset["train"]
+eval_dataset = split_dataset["test"]
 
 model = AutoModelForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=2)
 
@@ -30,14 +29,13 @@ training_args = TrainingArguments(
     output_dir="./results",
     evaluation_strategy="epoch",
     learning_rate=2e-5,
-    per_device_train_batch_size=16,
+    per_device_train_batch_size=4,
     num_train_epochs=3,
     weight_decay=0.01,
     logging_dir="./logs",
+    logging_steps=10,
+    save_strategy="epoch"
 )
-
-# Split the dataset into training and evaluation sets
-train_dataset, eval_dataset = train_test_split(tokenized_dataset, test_size=0.2)
 
 trainer = Trainer(
     model=model,
